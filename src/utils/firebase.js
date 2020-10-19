69fb4d0b5d9b857119ecc2ac8a38ad2042a600d2
kranks40 <eng_peters20@yahoo.com>
2020-10-19 10:37:34 -0400

    Initail app cleanup

---
+++ src/utils/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBlSeVHUj2Zd_J_aZeoXQ8OAgQS52UE5GU",
+    authDomain: "telegram-app-abcf2.firebaseapp.com",
+    databaseURL: "https://telegram-app-abcf2.firebaseio.com",
+    projectId: "telegram-app-abcf2",
+    storageBucket: "telegram-app-abcf2.appspot.com",
+    messagingSenderId: "468884544679",
+    appId: "1:468884544679:web:18f09f302e48b4fa680f82",
+    measurementId: "G-GEB9ETDKYY"
+  };
